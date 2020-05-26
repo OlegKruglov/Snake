c77403163b99b9d85260c7252ce0adf108065694
алекс <@DESKTOP-TVQ711I>
2020-05-26 19:45:08 +0300

    initial commit

---
+++ Snake Kruglov/Sounds.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace Snake_Kruglov
+{
+    public class Sounds
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+        private string pathToMedia;
+        public Sounds(string pathToResources)
+        {
+            pathToMedia = pathToResources;
+        }
+
+        public void Play()
+        {
+            player.URL = pathToMedia + "Jotaro.wav";
+            player.settings.volume = 30;
+            player.controls.play();
+        }
+        public void Play(string songName)
+        {
+            player.URL = pathToMedia + songName + ".wav";
+            player.controls.play();
+        }
+    }
+
+}
